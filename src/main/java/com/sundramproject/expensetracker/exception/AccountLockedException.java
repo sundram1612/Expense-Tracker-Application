b0d45aa06099eeccdd591df52bd3b29d6dfd5bb9
Sundram Kumar <146369690+sundram1612@users.noreply.github.com>
2026-04-16 14:33:57 +0530

    Delete src/exception directory

--- src/main/java/com/sundramproject/expensetracker/exception/AccountLockedException.java
+++
@@ -1,7 +0,0 @@
-package com.sundramproject.expensetracker.exception;
-
-public class AccountLockedException extends RuntimeException {
-    public AccountLockedException(String message) {
-        super(message);
-    }
-}
